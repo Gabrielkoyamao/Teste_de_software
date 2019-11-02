d00c316c139a5ebc778e232b93e146a5799dbd9d
gabrielkoyamao <gabriel.koyama.alves@hotmail.com>
2019-11-02 15:23:33 -0300

    Aula jacoco

---
+++ 2bimestre/app/src/test/java/app/Calculadora.java
@@ -0,0 +1,17 @@
+package app;
+
+public class Calculadora {
+	
+	public Float dividir(Float x, Float y) {
+		
+		Float returnedVal;
+		try {
+			returnedVal = x/y;
+			if(y.equals(0.0f))
+				throw new ArithmeticException();
+		} catch (ArithmeticException ae) {
+			returnedVal = 0f;
+		}
+		return returnedVal;
+	}
+}
